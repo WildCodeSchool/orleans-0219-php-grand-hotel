2bc1fb75e6a5d90b43b0ceb1b329b247ab4f2887
Andre Dupin <dd.dupin@gmail.com>
2019-04-15 09:43:18 +0200

    formulaire avec vérifs

--- src/Model/AdminRoomManager.php
+++ src/Model/AdminRoomManager.php
@@ -5,6 +5,14 @@ namespace App\Model;
 
 class AdminRoomManager extends AbstractManager
 {
+
+
+    public $boum = [0, 1];
+    public $data = [];
+    public $postData = [];
+    public $errors = [];
+    public $numberOfCaracteristics = 6;
+
     /**
      *
      */
@@ -16,5 +24,76 @@ class AdminRoomManager extends AbstractManager
     public function __construct()
     {
         parent::__construct(self::TABLE);
+    }
+
+
+    public function checkName(array $postData, $data, array $errors): array
+    {
+        $postData['name'] = trim($postData['name']);
+        if ((strlen($postData['name']) < 1) || (strlen($postData['name']) > 50)) {
+            $errors['name'] = 'Le nom de la chambre ne peut pas être vide ou dépasser 50 caractères';
+            $this->errors['name'] = $errors['name'];
+            return $this->errors;
+        } else {
+            $data['name'] = $postData['name'];
+            $this->data['name'] = $data['name'];
+            return $this->data;
+        }
+    }
+
+    public function checkDescription(array $postData, $data, array $errors, string $nameInArray): array
+    {
+        $postData[$nameInArray] = trim($postData[$nameInArray]);
+        if (strlen($postData[$nameInArray]) < 1) {
+            $errors[$nameInArray] = 'La description ne peut pas être vide!';
+            $this->errors[$nameInArray] = $errors[$nameInArray];
+            return $this->errors;
+        } else {
+            $data[$nameInArray] = $postData[$nameInArray];
+            $this->data[$nameInArray] = $data[$nameInArray];
+            return $this->data;
+        }
+    }
+
+    public function checkNumber(array $postData, $data, array $errors, string $nameInArray): array
+    {
+        $postData[$nameInArray] = trim($postData[$nameInArray]);
+        if (($postData[$nameInArray] < 0) || (!is_numeric($postData[$nameInArray]))) {
+            $errors[$nameInArray] = 'Entrez un nombre supérieur à zéro.';
+            $this->errors[$nameInArray] = $errors[$nameInArray];
+            return $this->errors;
+        } else {
+            $data[$nameInArray] = $postData[$nameInArray];
+            $this->data[$nameInArray] = $data[$nameInArray];
+            return $this->data;
+        }
+    }
+
+    public function checkArea(array $postData, $data, array $errors, string $nameInArray): array
+    {
+        $postData[$nameInArray] = trim($postData[$nameInArray]);
+        if (($postData[$nameInArray] < 0) || (!is_numeric($postData[$nameInArray]))) {
+            $errors[$nameInArray] = 'Entrez un nombre supérieur à zéro.';
+            $this->errors[$nameInArray] = $errors[$nameInArray];
+            return $this->errors;
+        } else {
+            $data[$nameInArray] = $postData[$nameInArray];
+            $this->data[$nameInArray] = $data[$nameInArray];
+            return $this->data;
+        }
+    }
+
+    public function checkCaracteristics(array $postData, $data, array $errors, $nameInArray): array
+    {
+
+        if (strlen($postData[$nameInArray]) > 50) {
+            $errors[$nameInArray] = "50 caractères maximum";
+            $this->errors[$nameInArray] = $errors[$nameInArray];
+            return $this->errors;
+        } else {
+            $data[$nameInArray] = $postData[$nameInArray];
+            $this->data[$nameInArray] = $data[$nameInArray];
+            return $this->data;
+        }
     }
 }
