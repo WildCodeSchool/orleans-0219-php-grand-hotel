67f7c833a262b2535680bc8ac40f9dac54e61aee
Vang Jeannette <vangjeannette@gmail.com>
2019-04-30 13:53:01 +0200

    swiftmailer

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -13,8 +13,31 @@ class MailController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function index()
+    public function swiftMailer()
     {
+
+// Create the Transport
+      /*  $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465))
+            ->setUsername('legrandhotelorleans')
+            ->setPassword('chocolats')
+        ;
+
+// Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+
+// Create a message
+        $message = (new Swift_Message('Wonderful Subject'))
+            ->setFrom(['john@doe.com' => 'John Doe'])
+            ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
+            ->setBody('Here is the message itself')
+        ;
+
+// Send the message
+        $result = $mailer->send($message); */
+
+
+
+
         return $this->twig->render('MailController/index.html.twig');
     }
 }
